c7c696bad7341540fcc28b55eb5c2a99a85964f4
nheyn <nick@heyn.io>
2016-03-20 21:20:39 -0600

    Add Higher Order Component
    Add a higher-order Component function, that add styles to Components.

--- src/react-css.js
+++ src/react-css.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   Component: require('./Component'),
+  useStyles: require('./use-styles'),
   inline: require('./inline'),
   mixin: {
     css: require('./inline'),
---
+++ src/use-styles.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const React = require('react')
+
+const inline = require('./inline')
+
+/**
+ * Create a higher order Component that adds .css method (see Component.js) as a prop.
+ *
+ * @param Component The component to add 'css(..,)' to
+ *
+ * @return          The component with 'css(...)' added
+ */
+module.exports = (Compoent) => {
+  return React.createClass({
+    displayName: `UseStyles_${Compoent.displayName}`,
+
+    contextTypes: {
+      mixins: React.PropTypes.object
+    },
+
+    css(classes, declaredClasses) {
+      // Create inline function with dummy Component (that can return classes from a method)
+      const currInline = inline.bind({
+        props: this.props,
+        context: this.context,
+        classes() {
+          return classes
+        }
+      })
+
+      // Get styles
+      return currInline(declaredClasses)
+    },
+
+    render() {
+      return <Compoent {...this.props} css={this.css} />
+    }
+  })
+}
